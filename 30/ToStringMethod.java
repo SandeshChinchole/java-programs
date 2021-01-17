8ba82a54f8ea498af574e1fdbd94c537a122cf09
Sandesh Chinchole <sc31285n@pace.edu>
2021-01-16 23:46:42 -0500

    added code for directory 30 and 31

---
+++ 30/ToStringMethod.java
@@ -0,0 +1,17 @@
+
+public class ToStringMethod {
+
+	public static void main(String[] args) {
+
+		Vehicle vehicle = new Vehicle();
+		
+		// explicitly
+		
+		//System.out.println(vehicle.toString());
+		
+		// implicitly
+		
+		System.out.println(vehicle);
+	}
+
+}
