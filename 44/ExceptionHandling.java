2d19ce591db4c27b582c0c4834a10cd00f89fb69
Sandesh <31442991+SandeshChinchole@users.noreply.github.com>
2021-03-02 16:33:24 -0500

    included more exceptions

--- 44/ExceptionHandling.java
+++ 44/ExceptionHandling.java
@@ -1,12 +1,15 @@
 import java.util.Scanner;
 
+import java.util.InputMismatchException;
+
 public class ExceptionHandling {
 	
 	public static void main(String[] args) {
 		
+		Scanner scanner = new Scanner(System.in);
+
 		try {
-			Scanner scanner = new Scanner(System.in);
-		
+			
 			System.out.println("Enter a whole number to divide: ");
 			int x = scanner.nextInt();
 		
@@ -20,6 +23,16 @@ public class ExceptionHandling {
 		catch(ArithmeticException e) {
 			
 			System.out.println("You can't divide by zero");
+		}
+		catch(InputMismatchException e) {
+			
+			System.out.println("Please enter a number");
+		}
+		finally {
+			
+			//System.out.println("This will always execute");
+			
+			scanner.close();
 		}
 	}
 
