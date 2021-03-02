37f499a4c911ba2efc0e9ee491345e894260665c
Sandesh <31442991+SandeshChinchole@users.noreply.github.com>
2021-03-01 23:41:54 -0500

    added code for directory 44

---
+++ 44/ExceptionHandling.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class ExceptionHandling {
+	
+	public static void main(String[] args) {
+		
+		try {
+			Scanner scanner = new Scanner(System.in);
+		
+			System.out.println("Enter a whole number to divide: ");
+			int x = scanner.nextInt();
+		
+			System.out.println("Enter a whole number to divide by: ");
+			int y = scanner.nextInt();
+		
+			int z = x/y;
+		
+			System.out.println("Result: "+ z);
+		}
+		catch(ArithmeticException e) {
+			
+			System.out.println("You can't divide by zero");
+		}
+	}
+
+}
