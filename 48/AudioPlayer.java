58fd33a947f3574f35c109afeb92d1467f445734
Sandesh <31442991+SandeshChinchole@users.noreply.github.com>
2021-03-08 18:03:13 -0500

    improved code for 48

--- 48/AudioPlayer.java
+++ 48/AudioPlayer.java
@@ -1,18 +1,22 @@
 import java.io.File;
 import java.io.IOException;
-
+import java.util.Scanner;
 import javax.sound.sampled.*;
 
 public class AudioPlayer {
 
 	public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
 
+		Scanner scanner = new Scanner(System.in);
+		
 		File file = new File("theclimb.wav");
 		AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
 		Clip clip = AudioSystem.getClip();
 		clip.open(audioStream);
 		
 		clip.start();
+		
+		String response = scanner.next();
 	}
 
 }
